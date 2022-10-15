33ca4167feba0c7e5eb0cf27cbfd787f9b3f0ee9
Napat Sri-ampun <napatsriz@gmail.com>
2022-10-15 20:25:48 +0700

    Stack, Heap and Pointer

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,56 @@
 //     result
 // }
 
-fn times_two(number: i32) -> i32 {
-    number * 2
-}
+// fn times_two(number: i32) -> i32 {
+//     number * 2
+// }
 
 fn main() {
 
+    // Stack, Heap and Pointer
+    let my_number = 15; // This is an i32
+    let single_reference = &my_number; //  This is a &i32
+    let double_reference = &single_reference; // This is a &&i32
+    let five_references = &&&&&my_number; // This is a &&&&&i32
+    println!("{}", five_references);
+
     // Mutability & Shadowing
-    let mut my_number = 8;
-    my_number = 10;
+    // let mut my_number = 8;
+    // my_number = 10;
 
-    let my_number = 8; // This is an i32
-    println!("{}", my_number); // prints 8
-    let my_number = 9.2; // This is an f64 with the same name. But it's not the first my_number - it is completely different!
-    println!("{}", my_number); // Prints 9.2
+    // let my_number = 8; // This is an i32
+    // println!("{}", my_number); // prints 8
+    // let my_number = 9.2; // This is an f64 with the same name. But it's not the first my_number - it is completely different!
+    // println!("{}", my_number); // Prints 9.2
 
-    let my_number = 8; // This is an i32
-    println!("{}", my_number); // prints 8
-    {
-        let my_number = 9.2; // This is an f64. It is not my_number - it is completely different!
-        println!("{}", my_number) // Prints 9.2
-                                  // But the shadowed my_number only lives until here.
-                                  // The first my_number is still alive!
-    }
-    println!("{}", my_number); // prints 8
+    // let my_number = 8; // This is an i32
+    // println!("{}", my_number); // prints 8
+    // {
+    //     let my_number = 9.2; // This is an f64. It is not my_number - it is completely different!
+    //     println!("{}", my_number) // Prints 9.2
+    //                               // But the shadowed my_number only lives until here.
+    //                               // The first my_number is still alive!
+    // }
+    // println!("{}", my_number); // prints 8
 
-    let final_number = {
-        let y = 10;
-        let x = 9; // x starts at 9
-        let x = times_two(x); // shadow with new x: 18
-        let x = x + y; // shadow with new x: 28
-        x // return x: final_number is now the value of x
-    };
-    println!("The number is now: {}", final_number);
+    // let final_number = {
+    //     let y = 10;
+    //     let x = 9; // x starts at 9
+    //     let x = times_two(x); // shadow with new x: 18
+    //     let x = x + y; // shadow with new x: 28
+    //     x // return x: final_number is now the value of x
+    // };
+    // println!("The number is now: {}", final_number);
 
-    // Pretending we are using Rust without shadowing
-    let final_number = {
-        let y = 10;
-        let x = 9; // x starts at 9
-        let x_twice = times_two(x); // second name for x
-        let x_twice_and_y = x_twice + y; // third name for x!
-        x_twice_and_y // too bad we didn't have shadowing - we could have just used x
-    };
-    println!("The number is now: {}", final_number)
+    // // Pretending we are using Rust without shadowing
+    // let final_number = {
+    //     let y = 10;
+    //     let x = 9; // x starts at 9
+    //     let x_twice = times_two(x); // second name for x
+    //     let x_twice_and_y = x_twice + y; // third name for x!
+    //     x_twice_and_y // too bad we didn't have shadowing - we could have just used x
+    // };
+    // println!("The number is now: {}", final_number)
 
     // Display & Debug
     // let doesnt_print = ();
