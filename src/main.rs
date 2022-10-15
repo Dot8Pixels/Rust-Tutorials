a48b26a25164d9e7ef2d029b3fb2c9de92c03de4
Napat Sri-ampun <napatsriz@gmail.com>
2022-10-15 20:09:46 +0700

    Display & Debug

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,67 @@
-fn number() -> i32 {
-    8
-}
+// fn number() -> i32 {
+//     8
+// }
 
-fn multiply(number_one: i32, number_two: i32) -> i32 { // Two i32s will enter the function. We will call them number_one and number_two.
-    let result = number_one * number_two;
-    println!("{} times {} is {}", number_one, number_two, result);
-    result
-}
+// fn multiply(number_one: i32, number_two: i32) -> i32 { // Two i32s will enter the function. We will call them number_one and number_two.
+//     let result = number_one * number_two;
+//     println!("{} times {} is {}", number_one, number_two, result);
+//     result
+// }
 
 fn main() {
 
+    // Display & Debug
+    let doesnt_print = ();
+    println!("This will not print: {:#?}", doesnt_print);
+
+    print!("This will not print a new line");
+    println!(" so this will be on the same line");
+
+    println!("The smallest i8 is {} and the biggest i8 is {}.", i8::MIN, i8::MAX); // hint: printing std::i8::MIN means "print MIN inside of the i8 section in the standard library"
+    println!("The smallest u8 is {} and the biggest u8 is {}.", u8::MIN, u8::MAX);
+    println!("The smallest i16 is {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
+    println!("The smallest u16 is {} and the biggest u16 is {}.", u16::MIN, u16::MAX);
+    println!("The smallest i32 is {} and the biggest i32 is {}.", i32::MIN, i32::MAX);
+    println!("The smallest u32 is {} and the biggest u32 is {}.", u32::MIN, u32::MAX);
+    println!("The smallest i64 is {} and the biggest i64 is {}.", i64::MIN, i64::MAX);
+    println!("The smallest u64 is {} and the biggest u64 is {}.", u64::MIN, u64::MAX);
+    println!("The smallest i128 is {} and the biggest i128 is {}.", i128::MIN, i128::MAX);
+    println!("The smallest u128 is {} and the biggest u128 is {}.", u128::MIN, u128::MAX);
+
     // Printing "Hello World"
-    println!("Hello, world!");
-    println!("Hello, world number {}!", 8);
-    println!("Hello, worlds number {} and {}!", 8, 9);
+    // println!("Hello, world!");
+    // println!("Hello, world number {}!", 8);
+    // println!("Hello, worlds number {} and {}!", 8, 9);
 
-    println!("Hello, world number {}!", number());
+    // println!("Hello, world number {}!", number());
 
-    multiply(8, 9); // We can give the numbers directly
-    let some_number = 10; // Or we can declare two variables
-    let some_other_number = 2;
-    multiply(some_number, some_other_number); // and put them in the function
+    // multiply(8, 9); // We can give the numbers directly
+    // let some_number = 10; // Or we can declare two variables
+    // let some_other_number = 2;
+    // multiply(some_number, some_other_number); // and put them in the function
 
-    let multiply_result = multiply(8, 9); // We used multiply() to print and to give the result to multiply_result
-    println!("{}",multiply_result);
+    // let multiply_result = multiply(8, 9); // We used multiply() to print and to give the result to multiply_result
+    // println!("{}",multiply_result);
 
-    let my_number = 8;
-    println!("Hello, number {}", my_number);
+    // let my_number = 8;
+    // println!("Hello, number {}", my_number);
 
-    let my_number = {
-        let second_number = 8;
-            second_number + 9 // No semicolon, so the code block returns 8 + 9.
-                              // It works just like a function
-        };
+    // let my_number = {
+    //     let second_number = 8;
+    //         second_number + 9 // No semicolon, so the code block returns 8 + 9.
+    //                           // It works just like a function
+    //     };
     
-        println!("My number is: {}", my_number);
+    //     println!("My number is: {}", my_number);
 
-    let my_number = {
-        let second_number = 8; // declare second_number,
-            second_number + 9; // add 9 to second_number
-                                   // but we didn't return it!
-                                   // second_number dies now
-        };
+    // let my_number = {
+    //     let second_number = 8; // declare second_number,
+    //         second_number + 9; // add 9 to second_number
+    //                                // but we didn't return it!
+    //                                // second_number dies now
+    //     };
         
-    println!("My number is: {:?}", my_number); // my_number is ()
+    // println!("My number is: {:?}", my_number); // my_number is ()
 
     // Type Inference
     // let small_number: u8 = 10;
