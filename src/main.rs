036172608c3e777b405660ef0bad128f679218d9
Napat Sri-ampun <napatsriz@gmail.com>
2022-10-15 20:19:04 +0700

    Mutability & Shadowing

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,67 @@
 //     result
 // }
 
+fn times_two(number: i32) -> i32 {
+    number * 2
+}
+
 fn main() {
 
+    // Mutability & Shadowing
+    let mut my_number = 8;
+    my_number = 10;
+
+    let my_number = 8; // This is an i32
+    println!("{}", my_number); // prints 8
+    let my_number = 9.2; // This is an f64 with the same name. But it's not the first my_number - it is completely different!
+    println!("{}", my_number); // Prints 9.2
+
+    let my_number = 8; // This is an i32
+    println!("{}", my_number); // prints 8
+    {
+        let my_number = 9.2; // This is an f64. It is not my_number - it is completely different!
+        println!("{}", my_number) // Prints 9.2
+                                  // But the shadowed my_number only lives until here.
+                                  // The first my_number is still alive!
+    }
+    println!("{}", my_number); // prints 8
+
+    let final_number = {
+        let y = 10;
+        let x = 9; // x starts at 9
+        let x = times_two(x); // shadow with new x: 18
+        let x = x + y; // shadow with new x: 28
+        x // return x: final_number is now the value of x
+    };
+    println!("The number is now: {}", final_number);
+
+    // Pretending we are using Rust without shadowing
+    let final_number = {
+        let y = 10;
+        let x = 9; // x starts at 9
+        let x_twice = times_two(x); // second name for x
+        let x_twice_and_y = x_twice + y; // third name for x!
+        x_twice_and_y // too bad we didn't have shadowing - we could have just used x
+    };
+    println!("The number is now: {}", final_number)
+
     // Display & Debug
-    let doesnt_print = ();
-    println!("This will not print: {:#?}", doesnt_print);
+    // let doesnt_print = ();
+    // println!("This will not print: {:#?}", doesnt_print);
 
-    print!("This will not print a new line");
-    println!(" so this will be on the same line");
+    // print!("This will not print a new line");
+    // println!(" so this will be on the same line");
 
-    println!("The smallest i8 is {} and the biggest i8 is {}.", i8::MIN, i8::MAX); // hint: printing std::i8::MIN means "print MIN inside of the i8 section in the standard library"
-    println!("The smallest u8 is {} and the biggest u8 is {}.", u8::MIN, u8::MAX);
-    println!("The smallest i16 is {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
-    println!("The smallest u16 is {} and the biggest u16 is {}.", u16::MIN, u16::MAX);
-    println!("The smallest i32 is {} and the biggest i32 is {}.", i32::MIN, i32::MAX);
-    println!("The smallest u32 is {} and the biggest u32 is {}.", u32::MIN, u32::MAX);
-    println!("The smallest i64 is {} and the biggest i64 is {}.", i64::MIN, i64::MAX);
-    println!("The smallest u64 is {} and the biggest u64 is {}.", u64::MIN, u64::MAX);
-    println!("The smallest i128 is {} and the biggest i128 is {}.", i128::MIN, i128::MAX);
-    println!("The smallest u128 is {} and the biggest u128 is {}.", u128::MIN, u128::MAX);
+    // println!("The smallest i8 is {} and the biggest i8 is {}.", i8::MIN, i8::MAX); // hint: printing std::i8::MIN means "print MIN inside of the i8 section in the standard library"
+    // println!("The smallest u8 is {} and the biggest u8 is {}.", u8::MIN, u8::MAX);
+    // println!("The smallest i16 is {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
+    // println!("The smallest u16 is {} and the biggest u16 is {}.", u16::MIN, u16::MAX);
+    // println!("The smallest i32 is {} and the biggest i32 is {}.", i32::MIN, i32::MAX);
+    // println!("The smallest u32 is {} and the biggest u32 is {}.", u32::MIN, u32::MAX);
+    // println!("The smallest i64 is {} and the biggest i64 is {}.", i64::MIN, i64::MAX);
+    // println!("The smallest u64 is {} and the biggest u64 is {}.", u64::MIN, u64::MAX);
+    // println!("The smallest i128 is {} and the biggest i128 is {}.", i128::MIN, i128::MAX);
+    // println!("The smallest u128 is {} and the biggest u128 is {}.", u128::MIN, u128::MAX);
 
     // Printing "Hello World"
     // println!("Hello, world!");
