7964ff8c044f41600927119bf5a91004a76265c6
Napat Sri-ampun <napatsriz@gmail.com>
2022-10-20 21:09:58 +0700

    More About Printing

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,88 @@
 
 fn main() {
 
+    // More about printing
+    // Note: this is print!, not println!
+    print!("\t Start with a tab\nand move to a new line");
+
+    // Note: After the first line you have to start on the far left.
+    // If you write directly under println!, it will add the spaces
+    println!("Inside quotes
+you can write over
+many lines
+and it will print just fine.");
+
+    println!("If you forget to write
+    on the left side, the spaces
+    will be added when you print.");
+
+    println!("Here are two escape characters: \\n and \\t");
+
+    println!("He said, \"You can find the file at c:\\files\\my_documents\\file.txt.\" Then I found the file."); // We used \ five times here
+    println!(r#"He said, "You can find the file at c:\files\my_documents\file.txt." Then I found the file."#);
+
+    let my_string = "'Ice to see you,' he said."; // single quotes
+    let quote_string = r#""Ice to see you," he said."#; // double quotes
+    let hashtag_string = r##"The hashtag #IceToSeeYou had become very popular."##; // Has one # so we need at least ##
+    let many_hashtags = r####""You don't have to type ### to use a hashtag. You can just use #.""####; // Has three ### so we need at least ####
+
+    println!("{}\n{}\n{}\n{}\n", my_string, quote_string, hashtag_string, many_hashtags);
+
+    let r#let = 6; // The variable's name is let
+    let mut r#mut = 10; // This variable's name is mut
+
+    println!("let = {}, mut = {}", r#let, r#mut);
+
+    println!("{:?}", b"This will look like numbers");
+
+    println!("{:?}", br##"I like to write "#"."##);
+
+    println!("{:X}", '행' as u32); // Cast char as u32 to get the hexadecimal value
+    println!("{:X}", 'H' as u32);
+    println!("{:X}", '居' as u32);
+    println!("{:X}", 'い' as u32);
+
+    println!("\u{D589}, \u{48}, \u{5C45}, \u{3044}"); // Try printing them with unicode escape \u
+
+    let number = 9;
+    let number_ref = &number;
+    println!("{:p}", number_ref);
+
+    let number = 555;
+    println!("Binary: {:b}, hexadecimal: {:x}, octal: {:o}", number, number, number);
+
+    let father_name = "Vlad";
+    let son_name = "Adrian Fahrenheit";
+    let family_name = "Țepeș";
+    println!("This is {1} {2}, son of {0} {2}.", father_name, son_name, family_name);
+
+    println!(
+        "{city1} is in {country} and {city2} is also in {country},
+but {city3} is not in {country}.",
+        city1 = "Seoul",
+        city2 = "Busan",
+        city3 = "Tokyo",
+        country = "Korea"
+    );
+
+    let letter = "a";
+    println!("{:ㅎ^11}", letter);
+
+    let title = "TODAY'S NEWS";
+    println!("{:-^30}", title); // no variable name, pad with -, put in centre, 30 characters long
+    let bar = "|";
+    println!("{: <15}{: >15}", bar, bar); // no variable name, pad with space, 15 characters each, one to the left, one to the right
+    let a = "SEOUL";
+    let b = "TOKYO";
+    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b); // variable names city1 and city2, pad with -, one to the left, one to the right
+    println!("{:-<15}{:->15}", a, b);
+
     // Stack, Heap and Pointer
-    let my_number = 15; // This is an i32
-    let single_reference = &my_number; //  This is a &i32
-    let double_reference = &single_reference; // This is a &&i32
-    let five_references = &&&&&my_number; // This is a &&&&&i32
-    println!("{}", five_references);
+    // let my_number = 15; // This is an i32
+    // let single_reference = &my_number; //  This is a &i32
+    // let double_reference = &single_reference; // This is a &&i32
+    // let five_references = &&&&&my_number; // This is a &&&&&i32
+    // println!("{}", five_references);
 
     // Mutability & Shadowing
     // let mut my_number = 8;
