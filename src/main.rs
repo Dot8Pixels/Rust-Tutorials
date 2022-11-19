aa46c29ed8b341c75d97d24e28c7e910496703ec
Napat Sri-ampun <napatsriz@gmail.com>
2022-11-19 20:46:20 +0700

    Mutable References

--- src/main.rs
+++ src/main.rs
@@ -18,22 +18,51 @@
 //     type_name::<T>()
 // }
 
-fn return_str() -> &str {
-    let country = String::from("Austria");
-    let country_ref = &country;
-    country_ref // ⚠️ Error because variable only live in function.
-}
+// fn return_str() -> &str {
+//     let country = String::from("Austria");
+//     let country_ref = &country;
+//     country_ref // ⚠️ Error because variable only live in function.
+// }
 
 fn main() {
 
-    // More on References
-    let country = String::from("Austria");
-    let ref_one = &country;
-    let ref_two = &country;
+    // Mutable References
+    let mut my_number = 8;
+    let num_ref = &mut my_number;
+    *num_ref += 10; // Use * to change the i32 value.
+    println!("{}", my_number);
 
-    println!("{}", ref_one);
+    let second_number = 800;
+    let triple_reference = &&&second_number;
+    println!("second_number is {}", second_number);
+    print!("triple_reference is {}", triple_reference);
+    println!("Second_number = triple_reference? {}", second_number == ***triple_reference);
 
-    let country = return_str();
+    // let mut number = 10;
+    // let number_ref = &number;
+    // let number_change = &mut number;
+    // *number_change += 10;
+    // println!("{}", number_ref); // ⚠️ case - cannot run
+
+    let mut number = 10;
+    let number_change = &mut number; // create a mutable reference
+    *number_change += 10; // use mutable reference to add 10
+    let number_ref = &number; // create an immutable reference
+    println!("{}", number_ref); // print the immutable reference
+
+    let country = String::from("Austria"); // Now we have a String called country
+    let country_ref = &country; // country_ref is a reference to this data. It's not going to change
+    let country = 8; // Now we have a variable called country that is an i8. But it has no relation to the other one, or to country_ref
+    println!("{}, {}", country_ref, country); // country_ref still refers to the data of String::from("Austria") that we gave it.
+
+    // // More on References
+    // let country = String::from("Austria");
+    // let ref_one = &country;
+    // let ref_two = &country;
+
+    // println!("{}", ref_one);
+
+    // let country = return_str();
 
     // Strings
     // let name = "서태지"; // This is a Korean name. No problem, because a &str is UTF-8.
