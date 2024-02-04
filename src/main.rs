2808dda83908cca8e789bb7bca841a514a010c4b
Dot8Pixels <napatsriz@gmail.com>
2024-02-04 20:41:40 +0700

    feat: :sparkles: Variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 1;
+    println!("{}", a);
+
+    /* for mutable -------------------------------------------------------------- */
+    let mut b = 2;
+    println!("{}", b);
+
+    b = 3;
+    println!("{}", b);
 }
